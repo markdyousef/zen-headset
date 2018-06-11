b6ac9a9c9c34d44563f162359e293b282b7d1134
markdyousef <markdyousef@gmail.com>
2018-06-11 12:08:25 -0700

    separated client and server testing envs

--- src/cloud/bigquery.test.js
+++ src/cloud/bigquery.test.js
@@ -1,6 +1,6 @@
 const BigQuery = require("@google-cloud/bigquery");
 const { getDataset, getTable, getData } = require("./bigquery");
-require('dotenv').config()
+require("dotenv").config();
 
 const { PROJECT_ID, DATASET_TEST, TABLE } = process.env;
 
@@ -12,22 +12,22 @@ describe("UNIT TEST - bigquery", () => {
   test("getDataset(bigquery, id)", done => {
     getDataset(bigquery, DATASET_TEST).then(dataset => {
       expect(dataset.id).toBe(DATASET_TEST);
-      _dataset = dataset
+      _dataset = dataset;
       done();
     });
   });
   test("getTable(dataset, id)", done => {
     getTable(_dataset, TABLE).then(table => {
-        expect(table.id).toBe(TABLE)
-        _table = table;
-        done();
+      expect(table.id).toBe(TABLE);
+      _table = table;
+      done();
     });
   });
   test("getData(table, maxResults)", done => {
-      getData(_table, 100).then(rows => {
-          expect(rows.length).toBe(100);
-          _rows = rows;
-          done();
-      })
-  })
+    getData(_table, 100).then(rows => {
+      expect(rows.length).toBe(100);
+      _rows = rows;
+      done();
+    });
+  });
 });
